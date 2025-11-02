4e6738a49b28e74e06976396995925eb38ffddef
starter-ns <allali.anes.pro.com>
2025-11-02 21:35:51 +0100

    RentalPropertyCard mediaqueries for desktop still need some edit

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom'
 export default function NotFound() {
   return (
     <section style={{textAlign:'center',padding:'40px 0'}}>
-      <h1 style={{fontSize:72,color:'#ff6060',margin:0}}>404</h1>
-      <p>Oops! The page you’re looking for doesn’t exist.</p>
-      <Link to="/" style={{color:'#ff6060',textDecoration:'underline'}}>Return to home page</Link>
+      <h1 style={{fontSize:288,color:'#ff6060',margin:0}}>404</h1>
+      <p style={{fontSize:36,color:'#ff6060',}}>Oops! The page you’re looking for doesn’t exist.</p>
+      <Link to="/" style={{color:'#000000ff',textDecoration:'underline'}}>Return to home page</Link>
     </section>
   )
 }
